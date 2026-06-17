c936c17b2a78d878f30790f1c6e95a4287d33ede
qisve <qisve@users.noreply.github.com>
2026-06-17 13:19:46 +0800

    fix: use json capacitor config

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-const config: CapacitorConfig = {
-  appId: 'com.gitmirror.app',
-  appName: 'GitMirror',
-  webDir: 'dist',
-  androidScheme: 'https',
-}
-export default config
